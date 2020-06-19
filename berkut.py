6f9c82638305f8128e4a32ef9664a4e10b03dc64
Vladislav Ginger <vladislav@MacBook-Air-Vladislav.local>
2020-06-19 22:15:26 +0700

    AZS Berkut parcing

--- berkut.py
+++ berkut.py
@@ -8,22 +8,23 @@ def find_price_berkut():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/79.0.3945.79 Safari/537.36'}
-
     reg = r'\d{2}.\d{2}'
-
     r = requests.get('http://azs-berkut.ru/price', headers=headers)
-    soup = BS(r.content, 'html.parser')
+    if r.status_code == 200:
+        soup = BS(r.content, 'html.parser')
+        res = soup.find('div', class_='container center')
+        global price
+        price = res.find_all(text=re.compile(reg))
+        global name
+        name = ['АИ-92', 'Эко-плюс 92', 'АИ-95', 'Эко-плюс 95', 'ДТ', 'ДТз']
 
-    res = soup.find('div', class_='container center')
-    global price
-    price = res.find_all(text=re.compile(reg))
-    global name
-    name = ['АИ-92', 'Эко-плюс 92', 'АИ-95', 'Эко-плюс 95', 'ДТ', 'ДТз']
+        i = -1
+        while i != 5:
+            i += 1
+            print(name[i] + ': ' + price[i])
+    else:
+        print(f"Ошибка {r.status_code}")
 
-    i = -1
-    while i != 5:
-        i += 1
-        print(name[i] + ': ' + price[i])
 
 
 def update_reg_92():
@@ -70,3 +71,5 @@ def run_berkut():
     update_reg_92()
     update_reg_95()
     update_diesel()
+
+find_price_berkut()
