9262f7647c2d77469ad80de485697ecf10dfc57c
Vladislav Ginger <vladislav@MacBook-Air-Vladislav.local>
2020-06-19 22:32:46 +0700

    Updator of data base

--- update_base.py
+++ update_base.py
@@ -1,42 +1,41 @@
 import azs_prime
 import berkut
+import energia
 import lukoil
 import gasprom_1
 import gasprom_2
-import rosneft
 import times
 
-print('---------------------')
-print('------АЗС Прайм------'
-      '\n')
-azs_prime.run_prime()
-print('---------------------')
-print('------АЗС Беркут-----'
-      '\n')
-berkut.run_berkut()
-print('---------------------')
-print('------АЗС Лукойл----- '
-      '\n')
-lukoil.run_lukoil()
-print('---------------------')
-print('---АЗС Газпром(АК)---'
-      '\n')
-gasprom_1.run_gasprom_1()
-print('---------------------')
-print('------АЗС Газпром----'
-      '\n')
-gasprom_2.run_gasprom_2()
-print('---------------------')
-print('------АЗС Роснефть---'
-      '\n')
-rosneft.run_rosneft()
-print('---------------------')
-print('-------АЗС Тайм------'
-      '\n')
-times.run_time()
-print('---------------------')
 
+def update():
+      print('---------------------')
+      print('------АЗС Прайм------'
+            '\n')
+      azs_prime.run_prime()
+      print('---------------------')
+      print('------АЗС Беркут-----'
+            '\n')
+      berkut.run_berkut()
+      print('---------------------')
+      print('------АЗС Лукойл----- '
+            '\n')
+      lukoil.run_lukoil()
+      print('---------------------')
+      print('---АЗС Газпром(АК)---'
+            '\n')
+      gasprom_1.run_gasprom_1()
+      print('---------------------')
+      print('------АЗС Газпром----'
+            '\n')
+      gasprom_2.run_gasprom_2()
+      print('---------------------')
+      print('-------АЗС Тайм------'
+            '\n')
+      times.run_time()
+      print('---------------------')
+      print('-----АЗС Энергия-----'
+            '\n')
+      energia.run_energia()
+      print('---------------------')
 
-
-
-
+update()
