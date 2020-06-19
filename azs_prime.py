7034db7f6171cf128fe853be28dae267db5c64b3
Vladislav Ginger <vladislav@MacBook-Air-Vladislav.local>
2020-06-19 22:09:44 +0700

    AZS Prime parser

--- azs_prime.py
+++ azs_prime.py
@@ -13,20 +13,23 @@ def find_price_prime():
     reg_2 = r'[АPД]\w+'
 
     r = requests.get('https://azs-prime.ru/azs-maps', headers=headers)
-    soup = BS(r.content, 'html.parser')
-    res = soup.find(id='azsnet-list-price')
+    if r.status_code == 200:
+        soup = BS(r.content, 'html.parser')
+        res = soup.find(id='azsnet-list-price')
 
-    global name
-    name = res.find_all(text=re.compile(reg_2))
+        global name
+        name = res.find_all(text=re.compile(reg_2))
 
-    global price
-    price = res.find_all(text=re.compile(reg))
+        global price
+        price = res.find_all(text=re.compile(reg))
 
-    i = -1
+        i = -1
+        while i != 5:
+            i += 1
+            print(name[i] + ' - ' + price[i])
 
-    while i != 5:
-        i += 1
-        print(name[i] + ' - ' + price[i])
+    else:
+        print(f"Ошибка {r.status_code}")
 
 
 def update_reg_92():
@@ -68,8 +71,22 @@ def update_diesel():
     cor.close()
 
 
+def update_reg_98():
+    con = sqlite3.connect('data_base.db')
+    cor = con.cursor()
+    column = 'reg_98'
+    res = price[3]
+    id = 'brand'
+    record_id = 'Прайм'
+    query = 'UPDATE map SET ' + column + '=' + str(res) + ' WHERE ' + id + " = '" + str(record_id) + "'"
+    cor.execute(query)
+    con.commit()
+    cor.close()
+
+
 def run_prime():
     find_price_prime()
     update_reg_92()
     update_reg_95()
     update_diesel()
+    update_reg_98()
