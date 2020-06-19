31d162693f262400f88c010ba635f56b315e058e
Vladislav Ginger <vladislav@MacBook-Air-Vladislav.local>
2020-06-19 22:25:41 +0700

    AZS Gasprom parcing

--- gasprom_2.py
+++ gasprom_2.py
@@ -11,26 +11,29 @@ def find_price_gasprom_2():
 
     reg = r'\d{2}.\d{2}₽'
 
-    r = requests.get(
-        'https://www.gpnbonus.ru/our_azs/?region_id=137&region_name=%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B8%D0%B1%D0%B8'
-        '%D1%80%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C&CenterLon=79.770236&CenterLat=55'
-        '.276272&city=%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B8%D0%B1%D0%B8%D1%80%D1%81%D0%BA&FUEL%5B%5D=176&FUEL%5B%5D'
-        '=177&PARAM_AZS%5B%5D=181&PARAM_AZS%5B%5D=182',
-        headers=headers)
-    soup = BS(r.content, 'html.parser')
+    r = requests.get('https://www.gpnbonus.ru/our_azs/?region_id=137&region_name=%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B8'
+                     '%D0%B1%D0%B8%D1%80%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C&CenterLon'
+                     '=79.770236&CenterLat=55.276272&city=%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B8%D0%B1%D0%B8%D1%80%D1'
+                     '%81%D0%BA&FUEL%5B%5D=176&FUEL%5B%5D=177&PARAM_AZS%5B%5D=181&PARAM_AZS%5B%5D=182&PARAM_AZS%5B%5D'
+                     '=192', headers=headers)
+    if r.status_code == 200:
+        soup = BS(r.content, 'html.parser')
 
-    res = soup.find('div', class_='oh pt10')
-    global price
-    price = res.find_all(text=re.compile(reg))
-    name = ['G-100', 'G-95', 'АИ-95', 'АИ-92', 'ДТ']
+        res = soup.find('div', class_='oh pt10')
+        global price
+        price = res.find_all(text=re.compile(reg))
+        name = ['G-100', 'G-95', 'АИ-95', 'АИ-92', 'ДТ']
 
-    newprice = [x[:-2] for x in price]
-    price = newprice
+        newprice = [x[:-2] for x in price]
+        price = newprice
 
-    i = -1
-    while i != 4:
-        i += 1
-        print(name[i] + ': ' + price[i])
+        i = -1
+        while i != 4:
+            i += 1
+            print(name[i] + ': ' + price[i])
+
+    else:
+        print(f"Ошибка {r.status_code}")
 
 
 def update_reg_92():
